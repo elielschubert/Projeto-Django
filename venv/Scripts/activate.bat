e12b780a9ae9537b9720fbcdd8baa1b846f0c20b
Eliel <elielschubert9@gmail.com>
2024-03-14 20:13:16 -0300

    updt

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\sce3jvl\OneDrive - Bosch Group\Eliel Schubert - Cópia\Projeto - Django\venv
+set VIRTUAL_ENV=C:\Users\eliel\OneDrive\Documentos\GitHub\Projeto-Django\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
